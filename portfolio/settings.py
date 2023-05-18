5426277a30947ac921d6501185b247d3a2a992c3
emilyalvae <emilisa23@gmail.com>
2023-05-17 23:03:53 -0500

    bucket s3

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -163,6 +163,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+AWS_ACCESS_KEY_ID ='AKIAQYMLQOZIR466JHVR'
+AWS_SECRET_ACCESS_KEY ='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'
+
+AWS_STORAGE_BUCKET_NAME ='bucket-port-1'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
 #AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 #AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 #AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
